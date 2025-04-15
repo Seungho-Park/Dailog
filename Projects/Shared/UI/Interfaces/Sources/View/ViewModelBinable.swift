1b8e90ee432631993f0a2350fee02886e27d4054
박승호 <p_vatzz@naver.com>
2025-04-15 15:32:52 +0900

    Commit

--- Projects/Shared/UI/Interfaces/Sources/View/ViewModelBinable.swift
+++ Projects/Shared/UI/Interfaces/Sources/View/ViewModelBinable.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 public protocol ViewModelBinable: NSObjectProtocol {
-    associatedtype ViewModelType: ViewModel
+    associatedtype ViewModelType
     
     var viewModel: ViewModelType! { get set }
     
