9b3f67a7ebaf377ba939b9aed0eea43b1dce9cea
박승호 <p_vatzz@naver.com>
2025-07-09 22:36:57 +0900

    # fix

--- Projects/Feature/Diary/Example/Sources/SceneDelegate.swift
+++ Projects/Feature/Diary/Example/Sources/SceneDelegate.swift
@@ -3,15 +3,11 @@ import NeedleFoundation
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var featurediaryComponent: FeatureDiaryComponent!
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        
-        registerProviderFactories()
-        featurediaryComponent = DiaryComponent()
     }
 }
 
--- Projects/Feature/Settings/Example/Sources/SceneDelegate.swift
+++ Projects/Feature/Settings/Example/Sources/SceneDelegate.swift
@@ -1,17 +1,14 @@
 import UIKit
 import NeedleFoundation
+import FeatureSettings
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    var featuresettingsComponent: FeatureSettingsComponent!
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        
-        registerProviderFactories()
-        featuresettingsComponent = SettingsComponent()
     }
 }
 
